a82e50cb43c529407f4fca46013dc73f8dbb7c4e
Sam <40403217+SamGawn@users.noreply.github.com>
2021-05-12 20:01:31 +0100

    first commit

---
+++ DataAccess.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using DataHelperLibrary;
+using DataHelperLibrary.Models;
+
+namespace DapperCrudNorthwind
+{
+    class DataAccess
+    {
+
+        /// <summary>
+        /// [1] SELECT - All customers from customers table
+        /// </summary>       
+
+
+        public List<CustomerModel> GetAllCustomers()
+        {
+            using IDbConnection connection = new SqlConnection(Tools.CnnValue("Northwind"));
+            {
+                return connection.Query<CustomerModel>($"select * from Customers").ToList();                
+            }
+        }
+
+        /// <summary>
+        /// [2] SELECT (with parameter usage) - All customers from city of X from customers table
+        /// </summary>
+
+        public List<CustomerModel> GetCustomerByCity(string city)
+        {
+            using IDbConnection connection = new SqlConnection(Tools.CnnValue("Northwind"));
+            {
+                return connection.Query<CustomerModel>($"select * from Customers where City = '{ city }'").ToList();
+                //return connection.Query<Person>("dbo.People_GetByLastName @LastName", new { City = city }).ToList();
+            }
+        }
+
+        /// <summary>
+        /// [3] INSERT (with parameter usage) - A full customer account
+        /// </summary>
+
+        public void InsertCustomer(string customerId, string companyName, string contactName, string contactTitle, string address, string city, string region, string postalCode, string country, string phone, string fax)
+        {
+            using IDbConnection connection = new SqlConnection(Tools.CnnValue("Northwind"));
+            {
+                // create list of people to be inserted
+                List<CustomerModel> customer = new List<CustomerModel>();
+                customer.Add(new CustomerModel
+                { 
+                        CustomerId = customerId, 
+                        CompanyName = companyName, 
+                        ContactName = contactName, 
+                        ContactTitle = contactTitle,
+                        Address = address,
+                        City = city,
+                        Region = region,
+                        PostalCode = postalCode,
+                        Country = country,
+                        Phone = phone,
+                        Fax = fax
+
+                    }
+                );                
+                connection.Execute("dbo.usp_Customer_Insert @CustomerId, @CompanyName, @contactName, @ContactTitle, @Address, @City, @Region, @PostalCode, @Country, @Phone, @Fax", customer);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataHelperLibrary.Models;
+
+namespace DapperCrudNorthwind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /// <summary>
+            /// [1] SELECT - All customers from customers table
+            /// </summary>   
+
+            //DataAccess db = new DataAccess();
+            //var output = db.GetAllCustomers();
+            //Console.WriteLine(output.Count);
+            //for (int i = 0; i < output.Count; i++)
+            //{
+            //    Console.WriteLine(output[i].CompanyName);
+            //}
+            //Console.ReadLine();
+
+            /// <summary>
+            /// [2] SELECT (with parameter usage) - All customers from city of X from customers table
+            /// </summary>
+
+
+            //DataAccess db = new DataAccess();
+            //var output = db.GetCustomerByCity("London");
+            //Console.WriteLine(output.Count);
+            //for (int i = 0; i < output.Count; i++)
+            //{
+            //    Console.WriteLine(output[i].CompanyName);
+            //}
+            //Console.ReadLine();
+
+
+            /// <summary>
+            /// [3] INSERT (with parameter usage) - All customers from city of X from customers table
+            /// </summary>
+
+            DataAccess db = new DataAccess();
+            db.InsertCustomer("GAWNS", "Chelsea FC", "Sam Gawn", "AppSupport", "Imperial Palace", "Borehamwood", "UK", "SG18 OLM", "England", "01903785914", "unknown");    
+        }
+    }
+}
